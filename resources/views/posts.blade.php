6e6998de3db28f1b00a6d1da1d774abc3d1bb207
Junaid Khan <mjks4545@gmail.com>
2022-01-05 21:54:44 -0500

    added blade view engine add collections understood cache better

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,27 +4,18 @@
 <link rel="stylesheet" href="/app.css" />
 
 <body>
-    <article>
-        <h1><a href="/post/my-first-post"> First Blog Post </a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, aspernatur laboriosam. Laboriosam voluptas ratione dolores eos commodi? Nesciunt praesentium
-            provident, iure, nisi quibusdam sed sunt sequi, porro doloribus ea sit.
-        </p>
-    </article>
-    <article>
-        <h1><a href="/post/my-second-post">Second Blog Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, aspernatur laboriosam. Laboriosam voluptas ratione dolores eos commodi? Nesciunt praesentium
-            provident, iure, nisi quibusdam sed sunt sequi, porro doloribus ea sit.
-        </p>
-    </article>
-    <article>
-        <h1><a href="/post/my-third-post">Third Blog Post</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, aspernatur laboriosam. Laboriosam voluptas ratione dolores eos commodi? Nesciunt praesentium
-            provident, iure, nisi quibusdam sed sunt sequi, porro doloribus ea sit.
-        </p>
-    </article>
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="post/{{$post->slug}}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {!! $post->body !!}
+            </div>
+        </article>
+    @endforeach
 </body>
 
 <script src="/app.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        "posts" => Post::all()
+    ]);
 });
 
 Route::get('post/{post_name}', function ($slug) {
-    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        return redirect("/");
-    }
-    $post = cache()->remember("post.{slug}", 1200, fn () => file_get_contents($path));
-    return view('post', ["post" => $post]);
+    return view('post', [
+        "post" => Post::find($slug)
+    ]);
 })->where("post", "A-z_\-+");

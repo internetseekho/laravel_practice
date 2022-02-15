aa2f2613dae3e6246f75833ed349e4b8b575a998
Junaid Khan <mjks4545@gmail.com>
2022-02-15 00:22:58 -0500

    update till categories

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,18 @@
 @extends("components.layout")
 
 @section("content")
-    <article>
-        <h1>
-            {{ $post->title }}
-        </h1>
-        <div>
-            {!! $post->body !!}
-        </div>
-    </article>
-    <a href="/">Go Back To Home</a>
+<article>
+    <h1>
+        {{ $post->title }}
+    </h1>
+    <p>
+        <a href="/categories/{{ $post->category->slug }}">
+            {{ $post->category->name }}
+        </a>
+    </p>
+    <div>
+        {!! $post->body !!}
+    </div>
+</article>
+<a href="/">Go Back To Home</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,21 @@
-
 @extends("components.layout")
 
 @section("content")
-    @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="post/{{$post->slug}}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <div>
-            {!! $post->body !!}
-        </div>
-    </article>
-    @endforeach
+@foreach ($posts as $post)
+<article>
+    <h1>
+        <a href="post/{{$post->slug}}">
+            {{ $post->title }}
+        </a>
+    </h1>
+    <p>
+        <a href="/categories/{{ $post->category->slug }}">
+            {{ $post->category->name }}
+        </a>
+    </p>
+    <div>
+        {!! $post->body !!}
+    </div>
+</article>
+@endforeach
 @endsection
